f87ba3ff42dda51b101917578c5418b5571c8f09
mohammed sikder <sikderismail@gmail.com>
2020-12-17 20:20:02 -0500

    create homepage div

---
+++ src/homepage.component.jsx
@@ -0,0 +1,19 @@
+import './homepage.style.css'
+
+const HomePage=()=>(
+    <div className='homepage'>
+        <h1>Welcome to my home page</h1>
+        <div className='directory-manu'>
+            <div className='manu-item'>
+                <div className='content'>
+                    <div className='title'>Hats</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+
+            </div>
+        </div>
+        
+    </div>
+)
+
+export default HomePage
