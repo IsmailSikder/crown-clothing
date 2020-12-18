43ea3d0ab23d37268cf3ebdd7cc117803d8363cd
mohammed sikder <sikderismail@gmail.com>
2020-12-17 20:55:32 -0500

    ecommerce structure for shoping

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,15 +1,40 @@
-import './homepage.style.css'
+import './homepage.style.scss'
 
 const HomePage=()=>(
     <div className='homepage'>
         <h1>Welcome to my home page</h1>
-        <div className='directory-manu'>
-            <div className='manu-item'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
                 <div className='content'>
                     <div className='title'>Hats</div>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
+            </div>
 
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>JACKETS</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>SHOES</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>WOMEN</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>MEN</div>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
             </div>
         </div>
         
